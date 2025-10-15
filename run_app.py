b5f63c8329cee82b7e0e9fbec1d49c4bb2a9ac57
dimendes90 <167574096+dimendes90@users.noreply.github.com>
2025-10-14 21:09:18 -0300

    Create run_app.py

---
+++ run_app.py
@@ -0,0 +1,17 @@
+#importa o arquivo principal
+import multiprocessing as mp
+
+# Fix global de certificados para urllib/ssl
+import os, ssl, certifi
+os.environ["SSL_CERT_FILE"] = certifi.where()
+ssl._create_default_https_context = lambda *a, **k: ssl.create_default_context(cafile=certifi.where())
+
+from app import main
+
+if __name__ == "__main__":
+    mp.freeze_support()
+    try:
+        mp.set_start_method("spawn")
+    except RuntimeError:
+        pass
+    main()
